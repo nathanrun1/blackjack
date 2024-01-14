5152f116aa48879eaf3a619a80c47d5d40c719ba
nathanrun1 <nathanmreilly@gmail.com>
2024-01-13 20:01:55 -0500

    initial commit

---
+++ blackjack_engine/blackjackbot.py
@@ -0,0 +1,48 @@
+from blackjack import *
+
+decisions = ["S", "H", "Su", "Sp", "D"]
+decision_types = ["hand_action", "bet_sizing", "insurance"]
+
+
+# hilo(cards) returns Hi-Lo blackjack running count for all provided dealt cards
+def hilo(cards):
+    count = 0
+    for card in cards:
+        bj_rank = blackjack_rank(card.rank_val())
+        if 2 <= bj_rank <= 6:
+            count += 1
+        elif bj_rank == 10:
+            count -= 1
+    return count
+
+
+def basic_strategy_choice(hand, dealer_show, choices):
+    hand_sum = blackjack_sum(hand.cards)
+    dealer_show_rank = blackjack_rank(dealer_show.rank_val())
+
+
+
+class Bot:
+    def __init__(self, **kwargs):
+        name = kwargs.get("name")
+        bankroll = kwargs.get("bankroll")
+        if bankroll:
+            self.bankroll = bankroll
+        else:
+            self.bankroll = DEFAULT_BANKROLL
+        if name:
+            self.name = name
+        else:
+            self.name = f"Bot{random.randint(0, 999)}"
+
+    def __str__(self):
+        return self.name
+
+    # decide(self, decision_type, choices, **kwargs) provides the bot's decision based on multiple factors
+    def decide(self, decision_type, choices, **kwargs):
+        if decision_type == "hand_action":
+            hands = kwargs.get("hands")
+            deck = kwargs.get("deck")
+            drawn_cards = deck.drawn
+            current_hand = next((h for h in hands if h.open))
+            dealer_showing = kwargs.get("dealer_show")
